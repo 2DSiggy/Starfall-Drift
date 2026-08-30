53aeeecd5fd6a9534450f8b83807e82cfa68cd18
2DSiggy <siggymaxwell@gmail.com>
2026-08-30 13:19:24 -0400

    Change access permissions i think

--- Content.Shared/Damage/Components/DamageableComponent.cs
+++ Content.Shared/Damage/Components/DamageableComponent.cs
@@ -39,7 +39,7 @@ public sealed partial class DamageableComponent : Component
     ///     If this data-field is specified, this allows damageable components to be initialized with non-zero damage.
     /// </remarks>
     [DataField]
-    [Access(typeof(DamageableSystem), Other = AccessPermissions.None)]
+    [Access(typeof(DamageableSystem), Other = AccessPermissions.Read)]
     public DamageSpecifier Damage = new();
 
     /// <summary>
@@ -50,14 +50,14 @@ public sealed partial class DamageableComponent : Component
     ///     dictionary.
     /// </remarks>
     [ViewVariables]
-    [Access(typeof(DamageableSystem), Other = AccessPermissions.None)]
+    [Access(typeof(DamageableSystem), Other = AccessPermissions.Read)]
     public Dictionary<ProtoId<DamageGroupPrototype>, FixedPoint2> DamagePerGroup = new();
 
     /// <summary>
     ///     The sum of all damages in the DamageableComponent.
     /// </summary>
     [ViewVariables]
-    [Access(typeof(DamageableSystem), Other = AccessPermissions.None)]
+    [Access(typeof(DamageableSystem), Other = AccessPermissions.Read)]
     public FixedPoint2 TotalDamage;
 
     [DataField("radiationDamageTypes")]
